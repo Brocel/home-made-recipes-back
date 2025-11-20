469c267fbdb567c691a5b245674b24587e30ea69
Brocel <geenens.prof@gmail.com>
2025-11-20 14:29:02 +0100

    clean poms

---
+++ app/src/test/java/com/example/hmrback/HomeMadeRecipesBackApplicationTest.java
@@ -0,0 +1,4 @@
+package com.example.hmrback;
+
+public class HomeMadeRecipesBackApplicationTest {
+}
---
+++ app/src/test/java/com/example/hmrback/auth/service/AuthenticationServiceTest.java
@@ -0,0 +1,4 @@
+package com.example.hmrback.auth.service;
+
+public class AuthenticationServiceTest {
+}
